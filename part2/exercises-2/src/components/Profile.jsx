da9f8a6b2dcdd8076501f0154e933d7f56d04499
ngarner98 <exmachina091098@gmail.com>
2023-11-18 13:31:51 -0600

    fix

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -2,26 +2,31 @@ import './styles.css';
 import Button from './Button';
 import ocean from './oceans.json';
 
-const listItem = ocean.map(ocean =>
-   <div key= {ocean.id} className= {`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
-      <img src= {ocean.image} alt= {ocean.name}
-      className='img'/>  
-   </div>
-   );
+
 
 function Profile()
 {
+
+   const listItem = ocean.map(ocean =>
+      <div key= {ocean.id} 
+      className= {`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+         <img src= {ocean.image} alt= {ocean.name}
+         className='img'/>  
+         <h1>{ocean.name}</h1>
+            <h3>Fun Facts:</h3>
+               <li>{ocean.fact1}</li>
+               <li>{ocean.fact2}</li>
+               <li>{ocean.fact3}</li>
+            <Button />
+         </div>
+      );
+
+      
    return(
       <>
-      <div>
-         <h3>Fun Facts:</h3>
-            <li>{ocean.fact1}</li>
-            <li>{ocean.fact2}</li>
-            <li>{ocean.fact3}</li>
-         <Button />
-      </div>
+
          <ul>
-         <h1>{ocean.name}</h1>
+         
             {listItem}
          </ul>
          
